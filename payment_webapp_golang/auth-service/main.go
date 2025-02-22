5df232ef067ee18af6b61fbfe941effcc6ccb573
Daniel de Almeida <d.joao@campus.fct.unl.pt>
2025-02-22 18:42:04 +0000

    adds golang payment service

---
+++ payment_webapp_golang/auth-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	//Respond with a simple welcome message
+	fmt.Fprintln(w, "Welcome to the login page")
+}
+
+func main() {
+	http.HandleFunc("/login", welcomeHandler) //root path for the service
+	fmt.Println("Auth Service is running on port 8081")
+	http.ListenAndServe(":8081", nil)
+}
