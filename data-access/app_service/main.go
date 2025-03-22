554b88895cb786a1f5700b2938ab724dfde693fe
Daniel de Almeida <d.joao@campus.fct.unl.pt>
2025-03-22 14:24:36 +0000

    mysql_access: implements insert

--- data-access/app_service/main.go
+++ data-access/app_service/main.go
@@ -67,6 +67,18 @@ func main() {
 	}
 
 	fmt.Printf("Album found: %v\n", alb)
+
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 func albumsByArtist(name string) ([]Album, error) {
@@ -111,4 +123,19 @@ func albumById(id int64) (Album, error) {
 	}
 
 	return alb, nil
+}
+
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	return id, nil
 }
